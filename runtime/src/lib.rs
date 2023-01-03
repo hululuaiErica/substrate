49d9dd3cbc59ee61ec9016437c6754cbd6770d5c
yangdong <yangdong@shouqianba.com>
2023-01-03 22:38:37 +0800

    first commit

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -265,6 +265,10 @@ impl pallet_sudo::Config for Runtime {
 impl pallet_template::Config for Runtime {
 	type Event = Event;
 }
+impl pallet_poe::Config for Runtime {
+	type MaxClaimLength = ConstU32<512>;
+	type Event = Event;
+}
 
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
@@ -283,6 +287,7 @@ construct_runtime!(
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template,
+		PoeModule: pallet_poe,
 	}
 );
 
